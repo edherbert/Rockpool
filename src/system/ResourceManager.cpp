d914db4629daf337709a81e4ff44208aee547781
edherbert <edwardherbert802@yahoo.com>
2017-07-17 15:55:49 +0100

    Finished the directory searching part of the resource manager.

--- src/system/ResourceManager.cpp
+++ src/system/ResourceManager.cpp
@@ -9,10 +9,10 @@ ResourceManager::~ResourceManager(){
 }
 
 void ResourceManager::checkResourceLocations(){
-    wxLongLong startTime = wxGetLocalTimeMillis();
-    std::cout << "---CHECKING RESOURCE LOCATIONS---" << std::endl;
+    std::cout << "---Checking Resource Locations---" << std::endl;
     wxArrayString resourceLocations = main->getResourceListItems();
 
+    locations.clear();
     if(resourceLocations.size() == 0){
         std::cout << "There are no resource locations to check." << std::endl;
         return;
@@ -24,57 +24,26 @@ void ResourceManager::checkResourceLocations(){
         wxDir dir(path);
         if(!dir.IsOpened())return;
 
-        //Check if this resource location is already in the locations list
-        bool locationExistsAlready = false;
-        resourceLocation *location;
-        /*for(resourceLocation *r : locations){
-            if(r->path == i){
-                std::cout << "LOCATION EXISTS" << std::endl;
-                locationExistsAlready = true;
-                location = r;
-                break;
-            }
-        }*/
-        for(int r = 0; r < locations.size(); r++){
-            if(locations.at(r)->path == i){
-                std::cout << "LOCATION EXISTS" << std::endl;
-                locationExistsAlready = true;
-                location = locations.at(r);
-                break;
-            }
-        }
-        if(!locationExistsAlready){
-            std::cout << "LOCATION DOESNT EXISTS" << std::endl;
-            location = new resourceLocation;
-            location->path = i;
-        }
+        resourceLocation *location = new resourceLocation;
+        location->path = i;
 
-        std::cout << location << std::endl;
-
-        //It could be something to do with this bit here.
-        //Maybe once the resources are pushed the old ones are removed, because of garbage collection.
         wxString filename;
         bool cont = dir.GetFirst(&filename, wxEmptyString, wxDIR_FILES);
         while(cont){
             location->resources.push_back(filename);
             cont = dir.GetNext(&filename);
         }
-
-        if(!locationExistsAlready){
-            locations.push_back(location);
-        }
+        locations.push_back(location);
     }
 
     wxLongLong endTime = wxGetLocalTimeMillis();
 
-    for(resourceLocation *r : locations){
+    /*for(resourceLocation *r : locations){
         for(wxString s : r->resources){
-            //std::cout << s << std::endl;
+            std::cout << s << std::endl;
         }
-    }
-
-    std::cout << "Time taken: " << endTime - startTime << " Milliseconds" << std::endl << " " << std::endl;
-
+    }*/
+    std::cout << "Done Checking Resource Locations." << std::endl;
 }
 
 /*
