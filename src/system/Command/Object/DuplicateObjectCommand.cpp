5da6bc7e3c295e11ccaa8e9189a441b39eeff2e9
edherbert <edwardherbert802@yahoo.com>
2017-08-20 00:16:30 +0100

    Implemented the indexing into the duplicate command. Also fixed a bug where items were only being appended to the root.

--- src/system/Command/Object/DuplicateObjectCommand.cpp
+++ src/system/Command/Object/DuplicateObjectCommand.cpp
@@ -14,6 +14,8 @@ DuplicateObjectCommand::DuplicateObjectCommand(HierarchyTree *tree, wxArrayTreeI
 
         info.originItem = tree->getId(items[i]);
 
+        info.index = tree->getItemIndex(tree->GetItemParent(items[i]), items[i]);
+
         itemInfo.push_back(info);
         idCount++;
 
@@ -50,7 +52,9 @@ DuplicateObjectCommand::~DuplicateObjectCommand(){
 void DuplicateObjectCommand::performAction(){
     for(int i = 0; i < itemInfo.size(); i++){
         int targetId;
+        //Determine the id
         if(itemInfo[i].parentId == -1){
+            //If parentId == -1 then that means that the item is a base item.
             targetId = -1;
         }else{
             targetId = itemInfo[itemInfo[i].parentId].newItem;
@@ -58,16 +62,29 @@ void DuplicateObjectCommand::performAction(){
 
         wxTreeItemId targetItem;
         if(targetId == -1){
-            targetItem = tree->GetRootItem();
+            //This means that the item is a base item, so check if it should be attached to the root, or to an itme.
+            if(itemInfo[i].originParentItem == -1){
+                targetItem = tree->GetRootItem();
+            }else{
+                targetItem = tree->getItem(itemInfo[i].originParentItem);
+            }
         }else{
+            //If none of that applies, then just get the id of the parent item.
             targetItem = tree->getItem(targetId);
         }
 
+        wxTreeItemId newItem;
+        //If it's a base item then insert needs to be run instead so that the index can be applied.
+        if(itemInfo[i].parentId == -1){
+            newItem = tree->InsertItem(targetItem, itemInfo[i].index, itemInfo[i].text);
+        }else{
+            newItem = tree->AppendItem(targetItem, itemInfo[i].text);
+        }
+
+        //Finally, determine if the item should be added or set.
         if(!ran){
-            wxTreeItemId newItem = tree->AppendItem(targetItem, itemInfo[i].text);
             itemInfo[i].newItem = tree->addItem(newItem);
         }else{
-            wxTreeItemId newItem = tree->AppendItem(targetItem, itemInfo[i].text);
             tree->setItem(itemInfo[i].newItem, newItem);
         }
     }
